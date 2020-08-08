9da1926e80072dd7b0efff307cf19bfc5579911f
Dima Salenko <dmytrosalenko32@gmail.com>
2020-08-07 22:01:01 -0400

    Refactored solution for the Task 5 to better fit the Task 5 P.2: Removed redundant classes, added Delegation pattern implementation and commented some methods and classes

--- task_5/solution.py
+++ task_5/solution.py
@@ -1,11 +1,11 @@
 # --- Day 5: Sunny with a Chance of Asteroids ---
-from enum import Enum
+from enum import Enum, unique
 import abc
-import inspect
 
-from task_2 import Command, CommandProducer, Computer, parse_sequence
+from task_2 import Command, Computer, parse_sequence
 
 
+@unique
 class OpCodeExtended(Enum):
     """Additional set of instructions for the OpCode class"""
     ADD = 1
@@ -16,6 +16,7 @@ class OpCodeExtended(Enum):
 
 
 class ParameterModes(Enum):
+    """Thus Enum contains different modes for the Command object parameters"""
     POSITION = 0
     IMMEDIATE = 1
 
@@ -46,7 +47,7 @@ class CommandParameter:
     object as the command arguments and utilized in the execute() method"""
     @property
     def value(self):
-        """This property returns either sel.value as the number or the value
+        """This property returns either self.value as the number or the value
          stored in the memory at address of `value` depending on the
          parameter mode"""
         if self._mode == ParameterModes.POSITION.value:
@@ -54,7 +55,7 @@ class CommandParameter:
         elif self._mode == ParameterModes.IMMEDIATE.value:
             return self._value
         else:
-            return None
+            raise ValueError('Unknown argument mode')
 
     def __init__(self, memory, value, mode):
         self._value = value
@@ -63,28 +64,31 @@ class CommandParameter:
 
 
 class ExtendedCommand(Command):
+    """Extension for the Command class. Supports argument modes"""
     # default number of command parameters
     COMMAND_LENGTH = 4
 
-    def __init__(self, extended_opcode, param_1_value, param_2_value,
-                 result_addr, memory):
+    def __init__(self, delegate, extended_opcode, param_1_value, param_2_value,
+                 result_addr):
+        self.delegate = delegate
         # if only opcode is given, fill the string with leading zeroes to the
         # size of command + 1 since opcode takes 2 digits
         opcode_string = f'%0{self.COMMAND_LENGTH + 1}d' % extended_opcode
         op_code = int(opcode_string[-2:])
         if param_1_value is not None:
             param_1_mode = int(opcode_string[-3])
-            param_1 = CommandParameter(memory, param_1_value, param_1_mode)
+            param_1 = CommandParameter(self.delegate.memory, param_1_value,
+                                       param_1_mode)
         else:
             param_1 = None
 
         if param_1_value is not None:
             param_2_mode = int(opcode_string[-4])
-            param_2 = CommandParameter(memory, param_2_value, param_2_mode)
+            param_2 = CommandParameter(self.delegate.memory, param_2_value,
+                                       param_2_mode)
         else:
             param_2 = None
-
-        super().__init__(op_code, param_1, param_2, result_addr, memory)
+        super().__init__(op_code, param_1, param_2, result_addr, None)
 
     @abc.abstractmethod
     def execute(self, *args, **kwargs):
@@ -96,8 +100,8 @@ class ExtendedAddCommand(ExtendedCommand):
         first_addend = self.param_1.value
         second_addend = self.param_2.value
         result = first_addend + second_addend
-
-        self.memory[self.result_addr] = result
+        # store the result in Computer memory
+        self.delegate.memory[self.result_addr] = result
 
 
 class ExtendedMultCommand(ExtendedCommand):
@@ -105,45 +109,39 @@ class ExtendedMultCommand(ExtendedCommand):
         first_factor = self.param_1.value
         second_factor = self.param_2.value
         result = first_factor * second_factor
-
-        self.memory[self.result_addr] = result
+        # store the result in Computer memory
+        self.delegate.memory[self.result_addr] = result
 
 
 class InputCommand(ExtendedCommand):
     COMMAND_LENGTH = 2
 
-    def __init__(self, extended_opcode, store_addr, memory, input_buffer):
-        self.input_buffer = input_buffer
-        self.store_address = store_addr
-
-        super().__init__(extended_opcode,
+    def __init__(self, delegate, extended_opcode, result_addr):
+        super().__init__(delegate,
+                         extended_opcode,
                          param_1_value=None,
                          param_2_value=None,
-                         memory=memory,
-                         result_addr=None)
+                         result_addr=result_addr)
 
     def execute(self, *args, **kwargs):
-        self.memory[self.store_address] = self.input_buffer.value
+        self.delegate.memory[self.result_addr] = self.delegate.input_buffer
 
 
 class OutputCommand(ExtendedCommand):
     COMMAND_LENGTH = 2
 
-    def __init__(self, extended_opcode, value_addr, memory, output_buffer):
-        self.output_buffer = output_buffer
-        self.value_addr = value_addr
-
-        super().__init__(extended_opcode,
+    def __init__(self, delegate, extended_opcode, result_addr):
+        super().__init__(delegate,
+                         extended_opcode,
                          param_1_value=None,
                          param_2_value=None,
-                         memory=memory,
-                         result_addr=None)
+                         result_addr=result_addr)
 
     def execute(self, *args, **kwargs):
-        self.output_buffer.value = self.memory[self.value_addr]
+        self.delegate.output_buffer = self.delegate.memory[self.result_addr]
 
 
-class ExtendedCommandProducer(CommandProducer):
+class ExtendedComputer(Computer):
     COMMAND_MAPPING = {
         OpCodeExtended.ADD.value: ExtendedAddCommand,
         OpCodeExtended.MULT.value: ExtendedMultCommand,
@@ -151,45 +149,6 @@ class ExtendedCommandProducer(CommandProducer):
         OpCodeExtended.OUTPUT.value: OutputCommand
     }
 
-    def __init__(self, memory, input_buffer, output_buffer):
-        self.input_buffer = input_buffer
-        self.output_buffer = output_buffer
-        self.command_pointer = 0
-        super().__init__(memory)
-
-    def _get_next_command(self):
-        while True:
-            op_code = int(str(self.memory[self.command_pointer])[-2:])
-            if op_code == OpCodeExtended.TERM.value:
-                return op_code,
-
-            command_length = self.COMMAND_MAPPING[op_code].COMMAND_LENGTH
-            yield tuple(self.memory[
-                  self.command_pointer:self.command_pointer + command_length
-                  ])
-            self.command_pointer += command_length
-
-    def command_generator(self):
-        for command_parameters in self._get_next_command():
-            op_code = int(str(command_parameters[0])[-2:])
-            if op_code == OpCodeExtended.TERM.value:
-                return
-            else:
-                command_class = self.COMMAND_MAPPING[op_code]
-
-                arglist = inspect.getfullargspec(command_class.__init__)
-                buffer_args = {}
-                if 'input_buffer' in arglist.args:
-                    buffer_args['input_buffer'] = self.input_buffer
-                if 'output_buffer' in arglist.args:
-                    buffer_args['output_buffer'] = self.output_buffer
-
-                yield command_class(*command_parameters,
-                                    self.memory,
-                                    **buffer_args)
-
-
-class ExtendedComputer(Computer):
     @property
     def input_buffer(self):
         return self._input_buffer.value
@@ -202,16 +161,63 @@ class ExtendedComputer(Computer):
     def output_buffer(self):
         return self._output_buffer.value
 
+    @output_buffer.setter
+    def output_buffer(self, value):
+        self._output_buffer.value = value
+
+    @property
+    def command_pointer(self):
+        return self._command_pointer
+
+    @command_pointer.setter
+    def command_pointer(self, value):
+        self._command_pointer = value
+
     def __init__(self, memory, input_buffer=None, output_buffer=None):
         super().__init__(memory)
+        self._command_pointer = 0
         self._output_buffer = OutputBuffer(output_buffer)
         self._input_buffer = InputBuffer(input_buffer)
-        self.command_producer = ExtendedCommandProducer(memory,
-                                                        self._input_buffer,
-                                                        self._output_buffer)
+
+    def _get_next_command(self):
+        """
+        Generator method to return the required number of arguments for the
+        command at self.command_pointer
+        :return: op_code if op_code == 99 to indicate the end of program,
+        otherwise will return arguments
+        """
+        while True:
+            op_code = int(str(self.memory[self.command_pointer])[-2:])
+            if op_code == OpCodeExtended.TERM.value:
+                # return op_code if the op_code denotes the end of program
+                return op_code,
+
+            # get the required number of arguments based on the op_code
+            command_length = self.COMMAND_MAPPING[op_code].COMMAND_LENGTH
+            yield tuple(self.memory[
+                  self.command_pointer:self.command_pointer + command_length
+                  ])
+            self.command_pointer += command_length
+
+    def command_generator(self):
+        """
+        Generator method to return a Command object for the execution based
+        on op_code and arguments returned by _get_next_command() method
+        """
+        for command_parameters in self._get_next_command():
+            op_code = int(str(command_parameters[0])[-2:])
+            if op_code == OpCodeExtended.TERM.value:
+                # stop execution if the op_code denotes the end of program
+                return
+            else:
+                command_class = self.COMMAND_MAPPING[op_code]
+                yield command_class(self, *command_parameters)
 
     def run_program(self):
-        for command in self.command_producer.command_generator():
+        """
+        Subsequently execute commands from the self.memory
+        """
+        for command in self.command_generator():
             command.execute()
 
 
