e488f2287e15c1b0da5e12cf58e509e5b8b315fc
Dima Salenko <dmytrosalenko32@gmail.com>
2020-08-09 22:08:04 -0400

    Added solution for the Task 9 P 2. That was easy

---
+++ task_9_part_2/solution.py
@@ -0,0 +1,18 @@
+from intcode_computer import IntcodeComputer, parse_program
+
+
+def solution(input_file_name):
+    # parse the sequence of commands and replace two elements with values
+    # according to the task
+    num_sequence = parse_program(input_file_name)
+    computer = IntcodeComputer(program=num_sequence)
+    computer.send_input_data(2)
+    # set the input value that we want to pass to the Input command
+    computer.run_program()
+    return computer.output_buffer.value
+
+
+if __name__ == '__main__':
+    input_file = '../task_9_part_2/task_9_part_2_input.txt'
+    result = solution(input_file)
+    print(result)
